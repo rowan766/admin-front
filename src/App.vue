c08898d0991663559c821bc68836a332cdc6e15a
张欢 <zhang_huan@cestc.cn>
2025-11-06 15:38:51 +0800

    feat:增加路由

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,11 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <el-button type="primary">主要按钮</el-button>
+    <el-button type="success">成功按钮</el-button>
+    <el-button type="info">信息按钮</el-button>
+    <el-button type="warning">警告按钮</el-button>
+    <el-button type="danger">危险按钮</el-button>
   </div>
   <HelloWorld msg="Vite + Vue" />
 </template>
